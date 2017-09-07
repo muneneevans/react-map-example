a23a32be6af53ff1c16bde6e049eb807c0778791
Evans Munene <munene.evansk@gmail.com>
2017-09-07 18:26:45 +0300

    Add: kenya geo map

--- src/app/Containers/WorldMap.js
+++ src/app/Containers/WorldMap.js
@@ -12,8 +12,8 @@ class WorldMap extends Component{
 
     projection(){
         return geoMercator()
-            .scale(100)
-            .translate([800/2,450 /2 ])
+            .scale(900)
+            .translate([800/4,450/2  ])
     }
 
 
@@ -36,32 +36,33 @@ class WorldMap extends Component{
     }
 
     render(){
+        console.log(this.state.worldData)
         return(
             <svg width={ 800 } height={ 450 } viewBox="0 0 1000 450">
-        <g className="countries">
-          {
-            this.state.worldData.map((d,i) => (
-              <path
-                key={ `path-${ i }` }
-                d={ geoPath().projection(this.projection())(d) }
-                className="country"
-                fill={ `rgba(38,50,56,${1 / this.state.worldData.length * i})` }
-                stroke="#FFFFFF"
-                strokeWidth={ 0.5 }
-              />
-            ))
-          }
-        </g>
-        <g className="markers">
-          <circle
-            cx={ this.projection()([8,48])[0] }
-            cy={ this.projection()([8,48])[1] }
-            r={ 10 }
-            fill="#E91E63"
-            className="marker"
-          />
-        </g>
-      </svg>
+              <g className="countries">
+                {
+                  this.state.worldData.map((d,i) => (
+                    <path
+                      key={ `path-${ i }` }
+                      d={ geoPath().projection(this.projection())(d) }
+                      className="country"
+                      fill={ `rgba(38,50,56,${1 / this.state.worldData.length * i})` }
+                      stroke="#FFFFFF"
+                      strokeWidth={ 0.5 }
+                    />
+                  ))
+                }
+              </g>
+              <g className="markers">
+                <circle
+                  cx={ this.projection()([8,48])[0] }
+                  cy={ this.projection()([8,48])[1] }
+                  r={ 10 }
+                  fill="#E91E63"
+                  className="marker"
+                />
+              </g>
+            </svg>
 
         );
     }
